1928399c8f3dae90685017d649c4329d1263ba0e
Steed <steed.monteiro@gmail.com>
2022-01-19 06:48:20 +0100

    fix: build

--- esm/index.js
+++
@@ -1,6 +0,0 @@
-export { View, SafeAreaView, ScrollView } from './View';
-export { Image, ImageBackground, RoundedImage, SquaredImage } from './Image';
-export { Text } from './Text';
-export { Horizontal, Layout, Inline, Vertical, Center, AlignStart, AlignCenter, AlignEnd, Start, End, } from './Layout';
-export { Icon } from './Icon';
-//# sourceMappingURL=index.js.map
