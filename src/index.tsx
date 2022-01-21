f4163e573f0f19ee10f24ad4c67168f183e3bf8c
Steed <steed.monteiro@gmail.com>
2022-01-21 15:00:51 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,3 +2,5 @@ export * from './View';
 export * from './Image';
 export * from './Text';
 export * from './Layout';
+export * from './provider/ResponsiveProvider';
+export * from './provider/ThemeProvider';
